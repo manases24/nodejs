4941f274da4515c2d1a5b0031d4e05f60095c183
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-11 19:16:32 +0000

    se agrega 'winston' para el logger

--- 03-bases-TS-node/src/plugins/index.js
+++ 03-bases-TS-node/src/plugins/index.js
@@ -1,11 +1,9 @@
 const {getUUID} = require('./get-id.plugin')
 const { getAge }  = require('./get-age.plugin');
 const { http } = require('./http-client.plugin');
-// import { buildLogger } from "./logger.plugin";
 
 module.exports = {
   getAge, 
   getUUID,
   http,
-  // buildLogger
 }
