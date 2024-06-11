8aa6e2ffb4ca87f096981a0949c18af3a1e26326
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-11 18:56:28 +0000

    initial commit

---
+++ 03-bases-TS-node/src/app.js
@@ -0,0 +1,28 @@
+const { getAge, getUUID } = require('./plugins');
+// const { emailTemplate } = require('./js-foundation/01-template')
+// const { proccessDes } = require('./js-foundation/02-destructuring')
+// const { getUserById } = require("./js-foundation/03-callbacks");
+// const { buildMakePerson } = require("./js-foundation/05-factory");
+const getPokemonById = require('./js-foundation/06-promises');
+
+// const id = 2;
+// getUserById(id, (error, user) => {
+//   if (error) {
+//     throw new Error(error);
+//   }
+//   console.log(user)
+// });
+
+// ! Referencia a la función factory y uso
+// const makePerson = buildMakePerson({ getUUID, getAge });
+// const obj = { name: 'John', birthdate: '1985-10-21' };
+// const john = makePerson( obj );
+// console.log({ john });
+
+
+
+
+getPokemonById(4)
+  .then( ( pokemon ) => console.log({ pokemon }) )
+  .catch( ( err ) => console.log( err ) )
+  .finally( () => console.log('Finalmente') );
