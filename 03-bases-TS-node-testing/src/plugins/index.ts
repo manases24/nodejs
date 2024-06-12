60878f991d838dc6b979706c05b1337b0ac38eea
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-12 21:46:04 +0000

    se agrega testing

--- 03-bases-TS-node-testing/src/plugins/index.ts
+++ 03-bases-TS-node-testing/src/plugins/index.ts
@@ -1,9 +1,9 @@
-const {getUUID} = require('./get-id.plugin')
-const { getAge }  = require('./get-age.plugin');
-const { http } = require('./http-client.plugin');
+const { getUUID } = require("./get-id.plugin");
+const { getAge } = require("./get-age.plugin");
+const { http } = require("./http-client.plugin");
 
 module.exports = {
-  getAge, 
+  getAge,
   getUUID,
   http,
-}
+};
