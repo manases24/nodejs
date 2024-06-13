33c61ab2294db9eaea44ec60e1f96685f5068c81
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-13 18:17:22 +0000

    refactoring

--- 04-node-TS-multiplication-app/src/app.logic.ts
+++ 04-node-TS-multiplication-app/src/app.logic.ts
@@ -1,20 +1,22 @@
-// import fs from "fs";
+import fs from "fs";
+import { yarg } from "./config/plugins/yargs.plugin";
 
-// let outputMessage = "";
-// const base = 5;
-// const headerMessage = `
-// ==================================
-//         Tabla del ${base}
-// ==================================\n
-// `;
+let outputMessage = "";
+const base = yarg.b;
+const headerMessage = `
+==================================
+        Tabla del ${base}
+==================================\n
+`;
 
-// for (let index = 1; index <= 10; index++) {
-//   outputMessage += `${base} x ${index} = ${base * index}\n`;
-// }
-// outputMessage = headerMessage + outputMessage;
-// console.log(outputMessage);
+for (let index = 1; index <= yarg.l; index++) {
+  outputMessage += `${base} x ${index} = ${base * index}\n`;
+}
+outputMessage = headerMessage + outputMessage;
 
-// const outputPath = `outputs/`;
-// fs.mkdirSync(outputPath, { recursive: true });
-// fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
-// console.log("File created");
+yarg.s && console.log(outputMessage);
+
+const outputPath = `outputs/`;
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+console.log("File created");
--- 04-node-TS-multiplication-app/src/app.ts
+++ 04-node-TS-multiplication-app/src/app.ts
@@ -1,9 +1,10 @@
 import { yarg } from "./config/plugins/yargs.plugin";
+import { ServerApp } from "./presentation/server.app";
 
 (async () => {
   await main();
 })();
 
 async function main() {
-  console.log(yarg);
+  ServerApp.run({ base: yarg.b, limit: yarg.l, showTable: yarg.s });
 }
