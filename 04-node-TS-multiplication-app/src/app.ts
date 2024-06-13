8908e6cb113e6b829dffe07064eefb45285478e8
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-13 13:02:05 +0000

    primeros pasos

--- 04-node-TS-multiplication-app/src/app.ts
+++ 04-node-TS-multiplication-app/src/app.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+import fs from "fs";
+
+let outputMessage = "";
+const base = 5;
+const headerMessage = `
+==================================
+        Tabla del ${base}
+==================================\n
+`;
+
+for (let index = 1; index <= 10; index++) {
+  outputMessage += `${base} x ${index} = ${base * index}\n`;
+}
+outputMessage = headerMessage + outputMessage;
+console.log(outputMessage);
+
+const outputPath = `outputs/`;
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+console.log("File created");
