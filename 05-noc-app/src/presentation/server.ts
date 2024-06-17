0c69c511e0aa1e20bb6c369b19dbc0318fd3f7f3
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-17 17:03:55 +0000

    CronService added

--- 05-noc-app/src/presentation/server.ts
+++ 05-noc-app/src/presentation/server.ts
@@ -1,5 +1,11 @@
+import { CronService } from "./cron/cron-service";
+
 export class Server {
   public static start() {
     console.log("Server started");
+    CronService.createJob("*/5 * * * * *", () => {
+      const date = new Date();
+      console.log("5 seconds", date);
+    });
   }
 }
