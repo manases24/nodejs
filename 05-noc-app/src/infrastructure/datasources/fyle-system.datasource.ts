54fc51be4858eb5693f2314de7a53a20953eef32
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-19 23:22:04 +0000

    new update added

--- 05-noc-app/src/infrastructure/datasources/fyle-system.datasource.ts
+++ 05-noc-app/src/infrastructure/datasources/fyle-system.datasource.ts
@@ -29,6 +29,16 @@ export class FileSystemDatasource implements LogDatasource {
     );
   };
 
+  private getLogsFromFile(path: string): LogEntity[] {
+    const content = fs.readFileSync(path, "utf-8");
+    const logs = content.split("\n").map(LogEntity.fromJson);
+    // const logs = content.split('\n').map(
+    //   log => LogEntity.fromJson(log)
+    // );
+
+    return logs;
+  }
+
   async saveLog(newLog: LogEntity): Promise<void> {
     const logAsJson = `${JSON.stringify(newLog)}\n`;
 
