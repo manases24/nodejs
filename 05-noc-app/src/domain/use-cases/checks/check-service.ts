9a4189702ff8e47ceb24afa881f0c8f4d2e45f77
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-20 00:26:30 +0000

    added new updates

--- 05-noc-app/src/domain/use-cases/checks/check-service.ts
+++ 05-noc-app/src/domain/use-cases/checks/check-service.ts
@@ -6,8 +6,8 @@ interface CheckServiceUseCase {
   execute(urls: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
@@ -22,7 +22,7 @@ export class CheckService implements CheckServiceUseCase {
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
-      this.successCallback();
+      this.successCallback && this.successCallback;
       const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
       this.logRepository.saveLog(log);
       return true;
@@ -33,7 +33,7 @@ export class CheckService implements CheckServiceUseCase {
         LogSeverityLevel.high
       );
       this.logRepository.saveLog(log);
-      this.errorCallback(errorMessage);
+      this.errorCallback && this.errorCallback(errorMessage);
       return false;
     }
   }
