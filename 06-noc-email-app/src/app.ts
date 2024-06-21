1bf8d524ef90e31e2cb8c178f1e43675066da604
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-21 17:41:19 +0000

    LogEntityOptions added

--- 06-noc-email-app/src/app.ts
+++ 06-noc-email-app/src/app.ts
@@ -7,6 +7,5 @@ import { Server } from "./presentation/server";
 })();
 
 function main() {
-  // Server.start();
-  // console.log(envs);
+  Server.start();
 }
--- 06-noc-email-app/src/domain/entities/log.entity.ts
+++ 06-noc-email-app/src/domain/entities/log.entity.ts
@@ -4,22 +4,30 @@ export enum LogSeverityLevel {
   high = "high",
 }
 
+export interface LogEntityOptions {
+  message: string;
+  level: LogSeverityLevel;
+  origin: string;
+  createdAt?: Date;
+}
+
 export class LogEntity {
   public level: LogSeverityLevel;
   public message: string;
+  public origin: string;
   public createdAt: Date;
 
-  constructor(message: string, level: LogSeverityLevel) {
-    this.message = message;
-    this.level = level;
+  constructor(options: LogEntityOptions) {
+    this.message = options.message;
+    this.level = options.level;
     this.createdAt = new Date();
+    this.origin = options.origin;
   }
 
   static fromJson = (json: string): LogEntity => {
-    const { message, level, createdAt } = JSON.parse(json);
+    const { message, level, createdAt, origin } = JSON.parse(json);
 
-    const log = new LogEntity(message, level);
-    log.createdAt = new Date(createdAt);
+    const log = new LogEntity({ message, level, origin, createdAt });
 
     return log;
   };
