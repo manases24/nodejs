48d66fbf0babea3259c2038d46d08a81b42b0bc6
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-24 21:17:04 +0000

    mongodb connect

--- 07-noc-email-db/src/app.ts
+++ 07-noc-email-db/src/app.ts
@@ -1,5 +1,6 @@
 import { envs } from "./config/plugins/envs.plugin";
 import { MongoDatabase } from "./data/mongo/init";
+import { LogModel } from "./data/mongo/models/log.model";
 import { Server } from "./presentation/server";
 
 (async () => {
@@ -11,5 +12,15 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
   });
-  Server.start();
+
+  const newLog = await LogModel.create({
+    message: "Test message from Mongo",
+    origin: "app.ts",
+    level: "low",
+  });
+
+  await newLog.save();
+  console.log(newLog);
+
+  // Server.start();
 }
