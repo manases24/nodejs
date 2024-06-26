11ae59bb480f515bc73d55f07bedbc2d8b096c95
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-26 13:09:36 +0000

    new update added

--- 07-noc-email-db/src/presentation/server.ts
+++ 07-noc-email-db/src/presentation/server.ts
@@ -1,13 +1,14 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
-import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource()
+const logRepository = new LogRepositoryImpl(
+  //new FileSystemDatasource()
+  new MongoLogDatasource()
 );
 const emailService = new EmailService();
 
@@ -15,10 +16,9 @@ export class Server {
   public static start() {
     console.log("Server started...");
 
-    new SendEmailLogs(emailService, fileSystemLogRepository)
-      .execute
-      //email address
-      ();
+    // new SendEmailLogs(emailService, fileSystemLogRepository).execute(
+    //   "hlk@gmail.com"
+    // );
     // emailService.sendEmailWithFileSystemLogs(
     //   ['fernando.herrera85@gmail.com','fernando.herrera.cr@gmail.com']
     // );
@@ -26,7 +26,7 @@ export class Server {
     CronService.createJob("*/5 * * * * *", () => {
       const url = "https://discord.com";
       new CheckService(
-        fileSystemLogRepository,
+        logRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
