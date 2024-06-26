d0f2e20e5524754d3562b2bc54ce9485da442cf0
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-26 13:04:23 +0000

    postgresql db config

--- 07-noc-email-db/src/config/plugins/envs.plugin.ts
+++ 07-noc-email-db/src/config/plugins/envs.plugin.ts
@@ -18,4 +18,5 @@ export const envs = {
   POSTGRES_USER: env.get("POSTGRES_USER").required().asString(),
   POSTGRES_PASSWORD: env.get("POSTGRES_PASSWORD").required().asString(),
   POSTGRES_DB: env.get("POSTGRES_DB").required().asString(),
+  POSTGRES_URL: env.get("POSTGRES_DB").required().asString(),
 };
