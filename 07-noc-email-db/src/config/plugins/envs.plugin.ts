80921719cf8d35203bea17ee02d13d4f9ccc23b2
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-26 13:03:36 +0000

    postgresql db config

--- 07-noc-email-db/src/config/plugins/envs.plugin.ts
+++ 07-noc-email-db/src/config/plugins/envs.plugin.ts
@@ -13,4 +13,9 @@ export const envs = {
   MONGO_PASSWORD: env.get("MONGO_PASSWORD").required().asString(),
   MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
   MONGO_URL: env.get("MONGO_URL").required().asString(),
+
+  // Postgres
+  POSTGRES_USER: env.get("POSTGRES_USER").required().asString(),
+  POSTGRES_PASSWORD: env.get("POSTGRES_PASSWORD").required().asString(),
+  POSTGRES_DB: env.get("POSTGRES_DB").required().asString(),
 };
