2852ec00ce0298db6a8a9c7bca246c4b40b1fa81
mnsh5 <hugo.orlando2050gmail.com>
2024-06-28 16:12:24 +0000

    NOC app added

--- 05-noc/src/domain/use-cases/checks/check-service.ts
+++ 05-noc/src/domain/use-cases/checks/check-service.ts
@@ -1,4 +1,4 @@
-import { LogSeverityLevel } from "./../../entities/log.entity";
+import { LogSeverityLevel } from "../../entities/log.entity";
 import { LogEntity } from "../../entities/log.entity";
 import { LogRepository } from "../../repository/log.repository";
 
