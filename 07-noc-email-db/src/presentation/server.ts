ecbc07e535d6402ecec334c75747e2036974134e
mnsh5 <hugo.orlando2050@gmail.com>
2024-06-28 16:02:47 +0000

    check service multiple added

--- 07-noc-email-db/src/presentation/server.ts
+++ 07-noc-email-db/src/presentation/server.ts
@@ -1,17 +1,25 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { FileSystemDatasource } from "../infrastructure/datasources/fyle-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
 import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-const logRepository = new LogRepositoryImpl(
-  //new FileSystemDatasource(),
-  // new MongoLogDatasource(),
+// const logRepository = new LogRepositoryImpl(
+//   new FileSystemDatasource(),
+//    new MongoLogDatasource(),
+//   new PostgresLogDatasource()
+// );
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const postgresLogRepository = new LogRepositoryImpl(
   new PostgresLogDatasource()
 );
+
 const emailService = new EmailService();
 
 export class Server {
@@ -27,8 +35,8 @@ export class Server {
 
     CronService.createJob("*/5 * * * * *", () => {
       const url = "https://discord.com";
-      new CheckService(
-        logRepository,
+      new CheckServiceMultiple(
+        [fsLogRepository, postgresLogRepository, mongoLogRepository],
         () => console.log(`${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
