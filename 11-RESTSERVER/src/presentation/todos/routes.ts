d0fb08cd4f1b32c1e7f9919558ce70ea2f2e33dd
mnsh5 <hugo.orlando2050gmail.com>
2024-06-29 18:52:53 +0000

    crud todo added

--- 11-RESTSERVER/src/presentation/todos/routes.ts
+++ 11-RESTSERVER/src/presentation/todos/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
-import { TodoController } from "./todos.controller";
+import { TodosController } from "./todos.controller";
 
 export class TodoRoutes {
   static get routes(): Router {
     const router = Router();
-    const todoController = new TodoController();
+    const todoController = new TodosController();
     router.get("/todos", todoController.getTodos);
     router.get("/todos/:id", todoController.getTodoById);
+    router.post("/todos", todoController.createTodo);
+    router.put("/todos/:id", todoController.updateTodo);
+    router.delete("/todos/:id", todoController.deleteTodo);
     return router;
   }
 }
