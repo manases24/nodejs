8f33b1a5065f97b8d02e0e890684f21ffff44c39
mnsh5 <hugo.orlando2050gmail.com>
2024-06-29 17:58:26 +0000

    env config added

--- 10-EXPRESS/src/main.ts
+++ 10-EXPRESS/src/main.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/envs.config";
 import { Server } from "./presentation/server";
 
 // Function anonima
@@ -6,6 +7,6 @@ import { Server } from "./presentation/server";
 })();
 
 function main() {
-  const server = new Server();
+  const server = new Server({ port: envs.PORT });
   server.start();
 }
--- 10-EXPRESS/src/presentation/server.ts
+++ 10-EXPRESS/src/presentation/server.ts
@@ -1,10 +1,23 @@
 import express from "express";
 
+interface Options {
+  port: number;
+}
 export class Server {
   private app = express();
+  private readonly port: number;
+
+  constructor(options: Options) {
+    this.port = options.port;
+  }
+
   async start() {
-    this.app.listen(3000, () => {
-      console.log(`Server running on port 3000`);
+    //* Middlewares
+    //* Public folder
+    this.app.use(express.static("public"));
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
     });
   }
 }
