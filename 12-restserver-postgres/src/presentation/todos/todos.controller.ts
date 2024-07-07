e57b60f97d28b7dc6f80b1753a6e62cd8e004b89
mnsh5 <hugo.orlando2050gmail.com>
2024-07-07 18:40:07 +0000

    updateTodo method updated

--- 12-restserver-postgres/src/presentation/todos/todos.controller.ts
+++ 12-restserver-postgres/src/presentation/todos/todos.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
-import { CreateTodoDto } from "../../domain/dtos";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 export class TodosController {
   //* DI
@@ -40,10 +40,9 @@ export class TodosController {
 
   public async updateTodo(req: Request, res: Response) {
     const id = +req.params.id;
-    if (isNaN(id))
-      return res
-        .status(400)
-        .json({ error: `ID argument [${id}] is not a number` });
+    const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+
+    if (error) return res.status(400).json(error);
 
     const todo = prisma.todo.findFirst({
       where: { id },
@@ -52,11 +51,9 @@ export class TodosController {
     if (!todo)
       return res.status(404).json({ error: `Todo with id ${id} not found` });
 
-    const { text, completedAt } = req.body;
-
     const updatedTodo = prisma.todo.update({
       where: { id },
-      data: { text, completedAt },
+      data: updateTodoDto!.values,
     });
     res.json(updatedTodo);
   }
